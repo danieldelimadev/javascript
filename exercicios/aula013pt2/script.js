ed6a55f5b01d7ce84bdea339b8dacd706fd00d03
Daniel dos Santos <154810817+danieldelimadev@users.noreply.github.com>
2024-02-23 06:03:26 -0300

    aula 13 complete

--- exercicios/aula013pt2/script.js
+++ exercicios/aula013pt2/script.js
@@ -8,20 +8,37 @@ function verificar() {
         var idade = data - Number(fano.value)
         var fsex = document.getElementsByName('radsex')
         var genero = ''
+        var foto = ''
         if (fsex[0].checked) {
-            var genero = 'Homem'
+                genero = 'Homem'
             if (idade > 0 && idade < 10) {
-                res.innerHTML += '<img src="bebeM" alt="bebe">'
+                foto = '<img src="imagens/bebeM.png" alt="bebe">'
             } else if (idade < 21) {
-
+                foto = '<img src="imagens/jovemM.png" alt="jovem">'
             } else if (idade < 50) {
-
+                foto = '<img src="imagens/adultoM.png" alt="adulto">'
+            } else if (idade <= 100) {
+                foto = '<img src="imagens/idosoM.png" alt="idoso">'
+            } else {
+                foto = '<img src="imagens/rip.png" alt="morto">'
             }
 
         } else {
             var genero = 'Mulher'
+            if (idade > 0 && idade < 10) {
+                foto = '<img src="imagens/bebeF.png" alt="bebe">'
+            } else if (idade < 21) {
+                foto = '<img src="imagens/jovemF.png" alt="jovem">'
+            } else if (idade < 50) {
+                foto = '<img src="imagens/adultoF.png" alt="adulta">'
+            } else if (idade <= 100) {
+                foto = '<img src="imagens/idosoF.png" alt="idosa">'
+            } else {
+                foto = '<img src="imagens/rip.png" alt="rip">'
+            }
         }
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML += foto
     }
     
 }
