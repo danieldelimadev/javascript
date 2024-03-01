5952c1b04fddae4f37a4f807894ba8e874fe9283
Daniel dos Santos <154810817+danieldelimadev@users.noreply.github.com>
2024-03-01 08:05:53 -0300

    Update objeto.js

--- exercicios/aula017/objeto.js
+++ exercicios/aula017/objeto.js
@@ -1,6 +1,20 @@
+/*
 let amigo = {nome: 'José', sexo: 'M', peso: 85.4, engordar(p=0){
     console.log('Engordou.')
     this.peso += p
 }}
 amigo.engordar(2)
 console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
+*/
+/*let array = [1,2,3,4]
+
+const somar = function(arr) {
+    valorTotal = 0
+    arr.forEach((value)=>{
+        valorTotal+=value
+    })
+    console.log(valorTotal)
+}
+
+somar(array)
+*/
