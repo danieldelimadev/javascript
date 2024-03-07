81af8983c31354d89b0659b40b6fad7545a35887
Daniel dos Santos <154810817+danieldelimadev@users.noreply.github.com>
2024-03-07 19:54:30 -0300

    update

--- desafios/analisador/index.js
+++ desafios/analisador/index.js
@@ -1,7 +1,7 @@
 var list = []
-let mulher = []
 
 function addpeople() {
+    res.innerHTML = ''
     let name = document.getElementById('name')
     let agetxt = document.getElementById('age')
     let sex = document.getElementsByName('radsex')
@@ -18,25 +18,34 @@ function addpeople() {
         sexo: gender
     }
     list.push(people)
-    //nome mulheres cadastradas
-    for (c in list) {
-        if (list[c].sexo == 'F') {
-            mulher.push(list[c].nome)
-        }
-    }
 }
 
 function analyse() {
     let res = document.getElementById('res')
     let sum = 0
+    let mulher = []
+    let older = []
     res.innerHTML = ''
     //soma das idades
     for (c in list) {
         sum += list[c].idade
+        
+        //mulheres cadastradas
+        if (list[c].sexo == 'F') {
+            mulher.push(list[c].nome)
+        }
+        //pessoas acima da média
+        if (list[c].idade > med) {
+            older.push(list[c].nome)
+        }
     }
-    //média 
-    let med = sum / list.length
+    
+    
     res.innerHTML += `<p>Foram adicionadas ${list.length} pessoas</p>`
-    res.innerHTML += `<p>A média de idade é ${med}</p>`
-    res.innerHTML += `<p>As mulheres cadastradas foram: ${mulher}</p>`
+    res.innerHTML += `<p>A média de idade é ${med.toFixed(2)}</p>`
+    if (mulher.length != 0){
+        res.innerHTML += `<p>As mulheres cadastradas foram: ${mulher}</p>`
+    }
+    res.innerHTML += `<p>Pessoas com idade acima da média: ${older}}</p>`
+
 }
